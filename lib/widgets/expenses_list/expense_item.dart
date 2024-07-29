2065090b1914420d76be1d1e7f473af5d29a3808
Vanya Awasthi <145616414+sheisstarwithoutmoon@users.noreply.github.com>
2024-07-29 15:02:01 +0530

    expense_item.dart

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -26,7 +26,7 @@ class ExpenseItem extends StatelessWidget {
             Row(
               children: [
                 Text(
-                  '\$${expense.amount.toStringAsFixed(2)}',
+                  'Rs. ${expense.amount.toStringAsFixed(2)}',
                 ),
                 const Spacer(),
                 Row(
